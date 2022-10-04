b0bdd3fe0a422c39de2da35ada890ef8491a5f78
jesue <jesue_gege@hotmail.com>
2022-10-04 20:19:30 -0300

    5 concluida

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-document.querySelectorAll('.color')[0].style.backgroundColor = 'black'
-document.querySelectorAll('.color')[1].style.backgroundColor = 'lightgreen'
-document.querySelectorAll('.color')[2].style.backgroundColor = 'lightblue'
-document.querySelectorAll('.color')[3].style.backgroundColor = 'lightYellow'
+window.onload = function () {
+    let todos = document.querySelectorAll('.color')
+    let teste = JSON.parse(localStorage.getItem('colorPalette'))
+    todos[1].style.backgroundColor = teste [0]
+    todos[2].style.backgroundColor = teste [1]
+    todos[3].style.backgroundColor = teste [2]
 
-function alteraCores (){
-let cores = 'xxx,xxx,xxx';
-for(index = 0; index < cores.length; index += 1){
-   if (index == 0 || index == 3 || index == 6){
-    cores = cores.replace('x', parseFloat((Math.ceil(Math.random() * 2))))
-   }else {
-    cores = cores.replace('x', parseFloat((Math.ceil(Math.random() * 5))))
-   }
-} return cores
+function trocaNaPagina() {
+    let todos = document.querySelectorAll('.color')
+
+    let primeiro = todos[1].style.backgroundColor = `rgb(${alteraCores()})`
+    let segundo = todos[2].style.backgroundColor = `rgb(${alteraCores()})`
+    let terceiro = todos[3].style.backgroundColor = `rgb(${alteraCores()})`  
+    let test = [];
+    test.push(primeiro, segundo, terceiro)
+    localStorage.setItem('colorPalette', JSON.stringify(test))
+}
+
+function alteraCores() {
+    let cores = 'xxx,xxx,xxx';
+    for (index = 0; index < cores.length; index += 1) {
+        if (index == 0 || index == 3 || index == 6) {
+            cores = cores.replace('x', parseFloat((Math.ceil(Math.random() * 2))))
+        } else {
+            cores = cores.replace('x', parseFloat((Math.ceil(Math.random() * 5))))
+        }
+    } return cores
 }
 let botao = document.querySelector('#button-random-color')
+botao.addEventListener('click', trocaNaPagina)
 
-botao.addEventListener('click', function(){
-    let todos = document.querySelectorAll('.color')
-    todos[1].style.backgroundColor = `rgb(${alteraCores()})`
-    todos[2].style.backgroundColor = `rgb(${alteraCores()})`
-    todos[3].style.backgroundColor = `rgb(${alteraCores()})`
-})
+}
