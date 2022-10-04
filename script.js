0f1d4c54c891faa5c42a5cfc640007ceab8f333d
jesue <jesue_gege@hotmail.com>
2022-10-04 15:32:32 -0300

    2 e 3 finalizada

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelectorAll('.color')[0].style.backgroundColor = 'black'
+document.querySelectorAll('.color')[1].style.backgroundColor = 'lightgreen'
+document.querySelectorAll('.color')[2].style.backgroundColor = 'lightblue'
+document.querySelectorAll('.color')[3].style.backgroundColor = 'lightYellow'
