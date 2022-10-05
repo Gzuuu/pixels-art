8aeb2a5e356a713492292ce75fd26bbb4767a65a
jesue <jesue_gege@hotmail.com>
2022-10-05 12:06:18 -0300

    6 e 7 finalizada

--- script.js
+++ script.js
@@ -31,4 +31,26 @@ function alteraCores() {
 let botao = document.querySelector('#button-random-color')
 botao.addEventListener('click', trocaNaPagina)
 
+function criaTabela (numero1, numero2){
+
+if (numero1 == undefined){numero1 = 5}
+if (numero2 == undefined){numero2 = 5}
+
+for (index = 0; index < numero1; index += 1){
+    let br = document.createElement('br')
+    let local = document.querySelector('#pixel-board')
+    local.appendChild(br)
+    br.setAttribute('class','brake')
+    for(index2 = 0; index2 < numero2; index2 += 1){
+    let div = document.createElement('div')
+    let local = document.querySelector('#pixel-board')
+    local.appendChild(div)
+    div.setAttribute('class', 'pixel')
+    div.style.display = 'inline-block'
+    div.style.backgroundColor = 'rgb(255, 255, 255)'
+    }
 }
+}
+criaTabela()
+
+}  
